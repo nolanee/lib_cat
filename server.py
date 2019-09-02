1ef25caec4b6d2eb5651a7f01b625bf4bc92e921
nolanee <nolanee@umich.edu>
2019-09-02 02:11:20 -0400

    added rudimentary web interface

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from library import Library
 
+@app.route('/')
+#@app.route('/?search=<search>')
+def search():
+    # url(?key=value)
+    s = request.args.get('search', '')
+
+    filename = 'new_lib_info.json'
+    lib = Library(filename, True)
+
+    # will want to get right from search in future
+    results = []
+    if s:
+        books = lib.search(s)
+        for book in books:
+            results.append(str(book))
+        if results == []:
+            results = -1
+
+    return render_template('index.html', query=s, results=results)
+    
 if __name__ == '__main__':
     app.run(debug=1)
